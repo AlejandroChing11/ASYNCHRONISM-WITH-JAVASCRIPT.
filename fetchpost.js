5a1df0a658a60f661eae25c82bb9675fa2a0d6a3
AlejandroChing11 <alejandroching2004@hotmail.com>
2022-10-05 18:28:46 -0500

    fetch post/delete

--- fetchpost.js
+++ fetchpost.js
@@ -28,3 +28,52 @@ const data = {
 postData(`${API}/products`, data)
     .then(response => response.json())
     .then(data => console.log(data));
+
+
+
+//Con PUT para actualizar un objeto
+function putData(urlApi, dataUpdate) {
+    const response = fetch(urlApi, {
+        method: 'PUT',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataUpdate)
+    });
+    return response;
+}
+
+const dataUpdate = {
+    "title": "Se puede cambiar tambien otras caracteristicas",
+    "price": 10 // no es necesario colocar todas las características del objeto, solo las que se cambiarán
+}
+
+putData(`${API}/products/271`, dataUpdate) //se debe colocar el id del objeto que se quiere modificar
+    .then(response => response.json())
+    .then(dataUpdate => console.log(dataUpdate));
+
+
+
+
+
+//Eliminar un objeto indicando el id con DELETE
+function deleteData(urlApi) { //no es necesario pasar la data
+    const response = fetch(urlApi, {
+        method: 'DELETE',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers:{
+            'Content-Type': 'application/json'
+        } //no es necesario especificar el body
+    });
+    return response;
+}
+
+const idNumber = 271; //se debe colocar el id del objeto qu se quiere modificar
+
+deleteData(`${API}/products/${idNumber}`) //no es necesario pasar data
+    .then(() => {
+        console.log(`Borrado ${idNumber}`); //es opcional imprimir en consola
+    });
